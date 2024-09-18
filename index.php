0afaa5657238252c7313dae5c9b34a0a9bb22f6c
jpsuarezu <juanpablosuarezurdaneta@gmail.com>
2024-09-18 12:44:10 -0300

    Nav / Boostrap
    Cambiando los nombres de las rutas / Cambiar los links del Boostrap

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$ruta = $_GET['s'] ?? 'home';
+$ruta = $_GET['s'] ?? 'inicio';
 
 $listaRutas = [
-    'home' => [
+    'inicio' => [
         'titulo' => 'TiendaTech',
     ],
     'productos' => [
@@ -32,8 +32,7 @@ if (!isset($listaRutas[$ruta])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 
@@ -41,11 +40,19 @@ if (!isset($listaRutas[$ruta])) {
 
         <nav>
 
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page"  href="index.php?s=inicio">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?s=productos">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?s=formulario">Contacto</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?s=datos_alumno">Datos de creador</a>
+                </li>
             </ul>
 
         </nav>
@@ -59,6 +66,8 @@ if (!isset($listaRutas[$ruta])) {
     <footer class="bg-dark text-white text-center py-3">
         <p>&copy; 2024 TiendaTech</p>
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
 </body>
 </html>
