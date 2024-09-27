40dd2dbceb8b39ee48c1c703a025d5bf09408c9d
jpsuarezu <juanpablosuarezurdaneta@gmail.com>
2024-09-27 15:15:02 -0300

    Css precio
    Agregando css al precio

--- vistas/productos.php
+++ vistas/productos.php
@@ -35,7 +35,7 @@
         <div class="pastilla">
             <img src="<?php echo $articulo->imagen; ?>" alt="<?php echo $articulo->imagen_descripcion; ?>">
             <h3><?php echo $articulo->titulo; ?></h3>
-            <p>Precio: <?php echo $articulo->precio; ?></p>
+            <p> <b> Precio: </b> <?php echo $articulo->precio; ?></p>
             <p><?php echo $articulo->descripcion; ?></p>
             <a href="index.php?s=producto&id=<?php echo $articulo->articulo_id; ?>" class="btn btn-primary">Ver más</a>
         </div>
