dc898e6060c77d565b86a86438a8229218df1a4b
jpsuarezu <juanpablosuarezurdaneta@gmail.com>
2024-11-08 11:01:17 -0300

    Agregando la vistas de sesiones
    Agrego las rutas para que pueda dirigir a las vistas de sesion y registro

---
+++ vistas/inicio_sesion.php
@@ -0,0 +1,19 @@
+<div class="formulario">
+
+<form action="/validar_sesion" method="POST">
+
+  <div class="mb-3">
+    <label for="NombreUsuario" class="form-label">Nombre del usuario</label>
+    <input type="text" class="form-control" id="NombreUsuario" name="NombreUsuario">
+  </div>
+
+  <div class="mb-3">
+    <label for="contraseña" class="form-label">contraseña</label>
+    <input type="password" class="form-control" id="contraseña" name="contraseña">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+
+</form>
+
+</div>
