0a545b7bc269232fed9a5e5dfc5677176fbc60a7
jpsuarezu <juanpablosuarezurdaneta@gmail.com>
2024-11-11 21:02:20 -0300

    Funcion de crear
    Funcion para la creacion de la parte de productos desde el backend

--- admin/acciones/agregar.php
+++ admin/acciones/agregar.php
@@ -3,20 +3,44 @@
 include 'conexion.php';
 
 
-$ArticuloCategoria = $_POST("Categoria");
-$ArticuloNombre = $_POST("Nombre");
-$ArticuloDescripcion = $_POST("Descripcion");
-$ArticuloImgDescripcion = $_POST("DescipcionImg");
-$ArticuloCantidad = $_POST("Cantidad");
+// $ArticuloCategoria = $_POST['Categoria'];
+// $ArticuloNombre = $_POST['Nombre'];
+// $ArticuloDescripcion = $_POST['Descripcion'];
+// $ArticuloImgDescripcion = $_POST['DescipcionImg'];
+// $ArticuloCantidad = $_POST['Cantidad'];
 
-$sql = "INSERT INTO products (categoria, titulo, descripcion, precio, imagen_descripcion, cantidad) VALUE ($ArticuloCategoria, $ArticuloNombre, $ArticuloDescripcion, $ArticuloImgDescripcion, $ArticuloCantidad')";
+// $sql = "INSERT INTO products (categoria, titulo, descripcion, precio, imagen_descripcion, cantidad) VALUE ('$ArticuloCategoria, $ArticuloNombre, $ArticuloDescripcion, $ArticuloImgDescripcion, $ArticuloCantidad')";
 
-$resultado = $conexion -> query($sql);
+// $resultado = $conexion -> query($sql);
 
-if ($resultado) {
-    header('Location: index.php');
-} else {
-    echo "No funciona";
-}
+// if ($resultado) {
+//     header('Location: index.php');
+// } else {
+//     echo "No funciona"; 
+// }
+
+if($_SERVER['REQUEST_METHOD'] === "POST")
+
+    $categoria = $_POST['Categoria'];
+    $nombre = $_POST['Nombre'];
+    $precio = $_POST['Precio'];
+    $descripcion = $_POST['Descripcion'];
+    $descripcionImg = $_POST['DescipcionImg'];
+    $cantidad = $_POST['Cantidad'];
+
+    $resultado = $conexion->prepare("INSERT INTO products (categoria, titulo, precio, descripcion, imagen_descripcion, cantidad) VALUE (:Categoria, :Nombre, :Precio, :Descripcion, :DescripcionImg, :Cantidad)");
+
+    $resultado->bindParam(":Categoria", $categoria);
+    $resultado->bindParam(":Nombre", $nombre);
+    $resultado->bindParam(":Precio", $precio);
+    $resultado->bindParam(":Descripcion", $descripcion);
+    $resultado->bindParam(":DescripcionImg", $descripcionImg);
+    $resultado->bindParam(":Cantidad", $cantidad);
+
+    if ($resultado->execute()){
+        header('location: ../vistas/Datos_Productos.php');
+    } else {
+        echo "error";
+    }
 
 ?>
--- admin/vistas/Agregar_Productos.php
+++ admin/vistas/Agregar_Productos.php
@@ -11,26 +11,30 @@
     
 <div class="container">
 
-<form action="/acciones/agregar.php" method="POST" enctype="multipart/form-data">
+<form action="../acciones/agregar.php" method="POST">
   <div class="mb-3">
     <label for="Categoria" class="form-label">Nombre de la categoria</label>
-    <input type="text" class="form-control" id="Categoria" name="Categoria">
+    <input type="text" class="form-control"  name="Categoria">
   </div>
   <div class="mb-3">
     <label for="Nombre" class="form-label">Nombre del titulo</label>
-    <input type="text" class="form-control" id="Nombre" name="Nombre">
+    <input type="text" class="form-control" name="Nombre">
+  </div>
+  <div class="mb-3">
+    <label for="Precio" class="form-label">Precio del producto</label>
+    <input type="number" class="form-control"  name="Precio">
   </div>
   <div class="mb-3">
     <label for="Descripcion" class="form-label">Descripcion del titulo</label>
-    <input type="text" class="form-control" id="Descripcion" name="Descripcion">
+    <input type="text" class="form-control"  name="Descripcion">
   </div>
   <div class="mb-3">
     <label for="DescipcionImg" class="form-label">Descripcion de la imagen</label>
-    <input type="text" class="form-control" id="DescipcionImg" name="DescipcionImg">
+    <input type="text" class="form-control"  name="DescipcionImg">
   </div>
   <div class="mb-3">
     <label for="Cantidad" class="form-label">Cantidad del producto</label>
-    <input type="number" class="form-control" id="Cantidad" name="Cantidad">
+    <input type="number" class="form-control"  name="Cantidad">
   </div>
   <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
